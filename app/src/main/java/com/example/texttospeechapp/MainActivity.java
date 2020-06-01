bef9b1c3b8aaa31cc5973e0c5c80352459a17d19
shobhitkrsks <sks.shobhit12@gmail.com>
2020-06-01 19:40:38 +0530

    added controls

--- app/src/main/java/com/example/texttospeechapp/MainActivity.java
+++ app/src/main/java/com/example/texttospeechapp/MainActivity.java
@@ -4,25 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.LocaleList;
+import android.os.Looper;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText textet;
     Button speakbtn;
     String text;
-    TextView localetv;
+    TextView localetv,pitchprog,speedprog;
+    SeekBar pitchbar,speedbar;
     TextToSpeech tts;
+
+    Button accentbtn[]=new Button[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +35,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textet=findViewById(R.id.textet);
-        speakbtn=findViewById(R.id.speakbtn);
         localetv=findViewById(R.id.localetv);
+
+        pitchbar=findViewById(R.id.pitchbar);
+        pitchprog=findViewById(R.id.pitchprog);
+;
+
+        speedbar=findViewById(R.id.speedbar);
+        speedprog=findViewById(R.id.speedprog);
+
+        accentbtn[0]=findViewById(R.id.uk);
+        accentbtn[1]=findViewById(R.id.us);
+        accentbtn[2]=findViewById(R.id.chinese);
+        accentbtn[3]=findViewById(R.id.french);
+        accentbtn[4]=findViewById(R.id.german);
+        accentbtn[5]=findViewById(R.id.italian);
+        accentbtn[6]=findViewById(R.id.korean);
+        accentbtn[7]=findViewById(R.id.taiwanese);
+        accentbtn[8]=findViewById(R.id.indian);
+        for(int i=0;i<9;i++)
+            accentbtn[i].setOnClickListener(this);
 
         tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(status==TextToSpeech.SUCCESS)
                 {
-//                    tts.setPitch(0.5f);
-//                    tts.setSpeechRate(0.8f);
+                    Toast.makeText(getApplicationContext(),"Initialisation Complete",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        speakbtn.setOnClickListener(new View.OnClickListener() {
+        pitchbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                String text=textet.getText().toString();
-                Set<Locale> availableLanguages = tts.getAvailableLanguages();
-                /*for(Locale locale:availableLanguages)
-                {
-                    if(locale.getDisplayLanguage().equals("English"))
-                    Log.e("Locale:",locale.getDisplayLanguage()+" "+locale.getDisplayCountry()+" "+locale.toString());
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int prog=progress;
+                prog-=50;
+                double p=1+((double)prog/50);
+                pitchprog.setText(String.format("%.1f",p)+"f");
+            }
 
-                }*/
-                startspeaking(text,Locale.CHINESE);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int prog=seekBar.getProgress();
+                prog-=50;
+                double p=1+((double)prog/50);
+                float pr=(float)p;
+                tts.setPitch(pr);
+            }
+        });
+
+        speedbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int prog=progress;
+                prog-=50;
+                double p=1+((double)prog/50);
+                speedprog.setText(String.format("%.1f",p)+"f");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int prog=seekBar.getProgress();
+                prog-=50;
+                double p=1+((double)prog/50);
+                float pr=(float)p;
+                tts.setSpeechRate(pr);
             }
         });
     }
 
-    private void startspeaking(String text,Locale locale) {
+    private void startspeaking(String text, final Locale locale) {
 
 
         tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
@@ -70,19 +124,35 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(getApplicationContext(),"Started"+utteranceId,Toast.LENGTH_SHORT).show();
+                        localetv.setText(locale.getDisplayName());
+                        pitchbar.setEnabled(false);
+                        speedbar.setEnabled(false);
+//                        Toast.makeText(getApplicationContext(),"Started"+utteranceId,Toast.LENGTH_SHORT).show();
                     }
                 });
             }
 
             @Override
-            public void onDone(String utteranceId) {
-                Toast.makeText(getApplicationContext(),"Completed"+utteranceId,Toast.LENGTH_SHORT).show();
+            public void onDone(final String utteranceId) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        localetv.setText("ACCENT");
+                        pitchbar.setEnabled(true);
+                        speedbar.setEnabled(true);
+//                        Toast.makeText(getApplicationContext(),"Completed"+utteranceId,Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
             @Override
-            public void onError(String utteranceId) {
-                Toast.makeText(getApplicationContext(),"Error"+utteranceId,Toast.LENGTH_SHORT).show();
+            public void onError(final String utteranceId) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),"Error"+utteranceId,Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
@@ -90,18 +160,59 @@ public class MainActivity extends AppCompatActivity {
         params.putString(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "");
 
         tts.setLanguage(locale);
-        localetv.setText(locale.getDisplayName());
         tts.speak(text,TextToSpeech.QUEUE_FLUSH,params,"");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         if(tts!=null)
         {
             tts.stop();
             tts.shutdown();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text=textet.getText().toString();
+        switch (v.getId())
+        {
+            case R.id.uk:
+                startspeaking(text,Locale.UK);
+                break;
+
+            case R.id.us:
+                startspeaking(text,Locale.US);
+                break;
+
+            case R.id.chinese:
+                startspeaking(text,Locale.CHINESE);
+                break;
+
+            case R.id.french:
+                startspeaking(text,Locale.FRENCH);
+                break;
+
+            case R.id.german:
+                startspeaking(text,Locale.GERMAN);
+                break;
+
+            case R.id.italian:
+                startspeaking(text,Locale.ITALIAN);
+                break;
+
+            case R.id.korean:
+                startspeaking(text,Locale.KOREAN);
+                break;
+
+            case R.id.taiwanese:
+                startspeaking(text,Locale.TAIWAN);
+                break;
+
+            case R.id.indian:
+                startspeaking(text,new Locale("en","IN"));
+                break;
         }
     }
 }
